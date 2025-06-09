47f1504421a9d4df1b15c0ed02cd1c487db5004b
josue <terrazasjosue22@gmail.com>
2025-06-08 23:08:45 -0400

    commit para agregar las urls en la aplicacion, modifique lista_cursos.html, agrege la url de accounts en la urls del proyecto

--- proy5_PlataformaCursosBreves/aplicacion/urls.py
+++ proy5_PlataformaCursosBreves/aplicacion/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('lista_cursos/', views.lista_cursos, name='lista_cursos'),
+    path('detalle_curso/<int:pk>/', views.detalle_curso, name='detalle_curso'),
+    path('crear_curso/', views.crear_curso, name='crear_curso'),
+    path('editar_curso/<int:pk>/editar/', views.editar_curso, name='editar_curso'),
+    path('eliminar_curso/<int:pk>/eliminar/', views.eliminar_curso, name='eliminar_curso'),
+    path("editar_material/<int:pk>/comentar/", views.editar_material, name="editar_material"),
+    path("subir_material/", views.subir_material, name="subir_material"),
+    path('inscribirse_curso/<int:curso_id>/', views.inscribirse_curso, name='inscribirse_curso'),
+    path('salirse_curso/<int:curso_id>/', views.salirse_curso, name='salirse_curso'),
+    path('', views.home, name='home'),
+    path('salir/', views.salir, name='salir'),
+    path('registrar/', views.registrar, name='registrar'),
 ]
