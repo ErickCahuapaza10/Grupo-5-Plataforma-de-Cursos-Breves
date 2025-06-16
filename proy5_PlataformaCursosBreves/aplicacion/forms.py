4a62f765c38f8a1b9251436140066cf2ac00e74d
Franco <francovillavicencio@gmail.com>
2025-06-15 21:49:42 -0400

    agregado los css del login,registro,detalle curso; se volvio responsivo en el base, distribucion grid

--- proy5_PlataformaCursosBreves/aplicacion/forms.py
+++ proy5_PlataformaCursosBreves/aplicacion/forms.py
@@ -4,14 +4,21 @@ from django.contrib.auth.models import User
 from .models import Profesor, Curso, Inscripcion, Material,PerfilUsuario,Entrega
 
 class RegistroForm(UserCreationForm):
-    rol = forms.ChoiceField(choices=PerfilUsuario.ROL_CHOICES, label="Tipo de usuario")
+    rol = forms.ChoiceField(
+        choices=PerfilUsuario.ROL_CHOICES,
+        label="Tipo de usuario"
+    )
     last_name = forms.CharField(label='Apellido', max_length=30, required=True)
+    first_name = forms.CharField(label='Nombre', max_length=30, required=True)
     email = forms.EmailField(required=True)
-
+    
     class Meta:
         model = User
-        fields = ['username', 'last_name', 'email','password1', 'password2', 'rol','first_name','last_name']
-        
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'rol']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'  
 class ProfesorForm(forms.ModelForm):
     class Meta:
         model = Profesor
